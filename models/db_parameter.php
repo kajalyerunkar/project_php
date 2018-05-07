cd79fd860a085e7213a0d4830521a88487e38e7a
kajalyerunkar <kajalyerunkar11@gmail.com>
2018-05-07 19:26:18 +0530

    07may2018

---
+++ models/db_parameter.php
@@ -0,0 +1,18 @@
+<?php 
+	 require_once "db_functions.php";
+	 interface db_parameter{
+	 	const HOSTNAME = "localhost";
+	 	const  USER= "root";
+	 	const  PASSWORD= "";
+	 	const DATABASE = "eshopper_project";
+	 }
+
+	 interface db_general_function{
+
+	function insert();
+ 	function update();
+ 	function delete();
+ 	function select();
+	 
+	 }
+?>
