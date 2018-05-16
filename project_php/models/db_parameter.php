0229992ad353796e5112bbd7986255319bf09d0a
kajalyerunkar <37833204+kajalyerunkar@users.noreply.github.com>
2018-05-16 10:12:22 +0530

    Add files via upload

--- project_php/models/db_parameter.php
+++ project_php/models/db_parameter.php
@@ -10,7 +10,7 @@
 	 	const HOSTNAME = "localhost";
 	 	const  USER= "root";
 	 	const  PASSWORD= "";
-	 	const DATABASE = "eshopper_project";
+	 	const DATABASE = "github";
 	 }
 
 	 interface db_general_function{
