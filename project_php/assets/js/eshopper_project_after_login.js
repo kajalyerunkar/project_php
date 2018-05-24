6b817daaf474a20f361dca3a8e4f2f0137d34349
kajalyerunkar <37833204+kajalyerunkar@users.noreply.github.com>
2018-05-24 13:28:02 +0530

    Add files via upload

--- project_php/assets/js/eshopper_project_after_login.js
+++ project_php/assets/js/eshopper_project_after_login.js
@@ -16,4 +16,33 @@ $(document).ready(function(){
 			$(".msg_brand").html(res);
 		})
 	})
+
+	$(".btn-product").click(function(){
+		//create an obj of given form
+		formobj=document.getElementById("product_form");
+		//alert(formobj);
+		//alert(1);
+		//create an obj of form data
+		dataobj = new FormData(formobj);
+		alert(dataobj);
+		$.ajax({
+			
+			data:dataobj,
+			url:"../controllers/product-action.php",
+			type:"post",
+			contentType:false,
+			processData:false,
+			success:function(response){
+				$(".msg_product").html(response);
+			},
+			error:function(errmsg){
+				console.log(errmsg);
+				}
+			});
+		// $.post("../controllers/product-action.php",record).success(function(res){
+		// 	$(".msg_product").html(res);
+		})
+
+
+
 })
